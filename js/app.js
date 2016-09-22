cf321bb94e9c1f76f230575aec01cf1a27e5e40f
John Wiltberger <johwiltb@gmail.com>
2016-09-22 16:17:05 -0700

    Commit site to github

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular.module('VPSSite', [])
+
+  .controller('VPSSiteController', VPSSiteController);
+
+  VPSSiteController.$inject = ['$scope', '$http'];
+  function VPSSiteController($scope, $http) {
+    $scope.data = "";
+    $scope.table = "";
+    $scope.displayData=true;
+
+    $scope.getVideos = function () {
+      $scope.displayData=false;
+      $http.get('videos.json')
+      .success(function(data, status, headers, config) {
+        // Called when the response is available
+        $scope.data = "Click the Links Below";
+        $scope.table = data;
+      })
+      .error(function(data, status, headers, config) {
+        $scope.data = "it failed :(";
+      })
+    }
+  }
+})();
